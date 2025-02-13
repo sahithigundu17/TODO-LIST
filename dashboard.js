e378843f19de70c2293692bf20a3c407a3543f45
Betelgeuse <b.telgeusedev@gmail.com>
2025-02-13 12:30:01 +0100

    finished home page

---
+++ dashboard.js
@@ -0,0 +1,6 @@
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const displayWeekDay = document.getElementById("day")
+const displayDayNb = document.getElementById("number")
+const displayMonth = document.getElementById("month")
